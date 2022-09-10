128e430aa3135a11e6e756f3ea9b047b9fcfa615
Aaron Moodie <hello@aaronmoodie.com>
2022-09-10 22:54:32 +1000

    added other pages

--- app/routes/quiz/correct.jsx
+++ app/routes/quiz/correct.jsx
@@ -1,10 +1,10 @@
 import { Link } from "@remix-run/react";
 
-export default function Index() {
+export default function NiceWork() {
 	return (
 		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-			<h1>Hi there!</h1>
-			<Link to="/quiz/question">You should click here!</Link>
+			<h1>You got it right!</h1>
+			<Link to="/">Back home</Link>
 		</div>
 	);
 }
---
+++ app/routes/quiz/question.jsx
@@ -0,0 +1,58 @@
+import { json, redirect } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
+import { useEffect, useRef } from "react";
+
+export function meta() {
+  return { title: "Actions Demo" };
+}
+
+export const action = async ({ request }) => {
+  let formData = await request.formData();
+  let answer = formData.get("answer");
+  
+  if (answer.length === 0) {
+    return json("Come on, at least try!", { status: 400 });
+  }
+  
+  if (answer !== "egg") {
+    return json(`Sorry, ${answer} is not right.`, { status: 400 });
+  }
+  
+  return redirect("/quiz/correct");
+}
+
+export default function Questions() {
+  let actionMessage = useActionData();
+  let answerRef = useRef(null);
+  
+  useEffect(() => {
+    if (actionMessage && answerRef.current) {
+      answerRef.current.select();
+    }
+  }, [actionMessage]);
+  
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Question time</h1>
+      
+      <Form method="post">
+        <p>
+          <i>What is more useful when it is broken?</i>
+        </p>
+        <label>
+          <div>Answer:</div>
+          <input ref={answerRef} name="answer" type="text" />
+        </label>
+        <div>
+          <button>Submit</button>
+        </div>
+        {actionMessage ? (
+          <p>
+            <b>{actionMessage}</b>
+          </p>
+        ) : null}
+      </Form>
+      
+    </div>
+  );
+}
